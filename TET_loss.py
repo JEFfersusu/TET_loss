6af5c2faf4843f9a67a765a5acc37a22c9160b76
JEFfersusu <142289262+JEFfersusu@users.noreply.github.com>
2026-01-06 11:38:40 +0800

    Update TET_loss.py

--- TET_loss.py
+++ TET_loss.py
@@ -1,14 +1,14 @@
-# class MiT_Loss(nn.Module):
-#     def __init__(self, T=0.5, lambda_entropy=0.1):
-#         super().__init__()
-#         self.ce = nn.CrossEntropyLoss()
-#         self.T = T
-#         self.lambda_entropy = lambda_entropy
+class MiT_Loss(nn.Module):
+    def __init__(self, T=0.5, lambda_entropy=0.1):
+        super().__init__()
+        self.ce = nn.CrossEntropyLoss()
+        self.T = T
+        self.lambda_entropy = lambda_entropy
 
-#     def forward(self, outputs, targets):
-#         scaled_outputs = outputs / self.T
-#         ce_loss = self.ce(scaled_outputs, targets)
-#         probs = torch.softmax(scaled_outputs, dim=1)
-#         entropy = - (probs * torch.log(probs + 1e-10)).sum(dim=1).mean()
+    def forward(self, outputs, targets):
+        scaled_outputs = outputs / self.T
+        ce_loss = self.ce(scaled_outputs, targets)
+        probs = torch.softmax(scaled_outputs, dim=1)
+        entropy = - (probs * torch.log(probs + 1e-10)).sum(dim=1).mean()
 
-#         return ce_loss - self.lambda_entropy * entropy
+        return ce_loss - self.lambda_entropy * entropy
